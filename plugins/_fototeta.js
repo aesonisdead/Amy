75c5024c1b9151273e3ce4b208c3b5018f256765
aesonisdead <vuuvuuvvuvybuu@gmail.com>
2025-10-23 19:50:00 +0100

    Update _fototeta.js

--- plugins/_fototeta.js
+++ plugins/_fototeta.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, command}) => {
-await conn.reply(m.chat,`Padre nuestro, que estás en los Cielos, santificado sea tu nombre, venga tu Reino, hágase tu voluntad así en la tierra como en el cielo. y perdónanos nuestras deudas así como nosotros perdonamos a nuestros deudores, y no nos dejes caer en la tentación, mas líbranos del mal.`, estilo)
+await conn.reply(m.chat,`Our Father, who art in heaven, hallowed be thy name, thy kingdom come, thy will be done on earth as it is in heaven. And forgive us our debts as we forgive our.`, estilo)
 }
 handler.customPrefix = /^(Fototeta|fototeta)$/i
 handler.command = new RegExp

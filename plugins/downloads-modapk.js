7f7b44f9d8059575d7aedb35043d4a00315efcc6
aesonisdead <vuuvuuvvuvybuu@gmail.com>
2025-10-24 11:54:39 +0100

    Update downloads-modapk.js

--- plugins/downloads-modapk.js
+++ plugins/downloads-modapk.js
@@ -1,28 +1,28 @@
 import { search, download } from 'aptoide-scraper'
 
 var handler = async (m, { conn, usedPrefix, command, text }) => {
-if (!text) return conn.reply(m.chat, `❀ Por favor, ingrese el nombre de la apk para descargarlo.`, m)
+if (!text) return conn.reply(m.chat, `❀ Please enter the name of the apk to download it..`, m)
 try {
 await m.react('🕒')
 let searchA = await search(text)
 let data5 = await download(searchA[0].id)
-let txt = `*乂  APTOIDE - DESCARGAS 乂*\n\n`
-txt += `≡ Nombre : ${data5.name}\n`
+let txt = `*乂  APTOIDE - DOWNLOADER 乂*\n\n`
+txt += `≡ Name : ${data5.name}\n`
 txt += `≡ Package : ${data5.package}\n`
 txt += `≡ Update : ${data5.lastup}\n`
-txt += `≡ Peso :  ${data5.size}`
+txt += `≡ Size :  ${data5.size}`
 await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m)
 if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-return await conn.reply(m.chat, `ꕥ El archivo es demasiado pesado.`, m)
+return await conn.reply(m.chat, `ꕥ The file is too large.`, m)
 }
 await conn.sendMessage(m.chat, { document: { url: data5.dllink }, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null }, { quoted: m })
 await m.react('✔️')
 } catch (error) {
 await m.react('✖️')
-return conn.reply(m.chat, `⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}report* para informarlo.\n\n${error.message}`, m)
+return conn.reply(m.chat, `⚠︎ A problem has occurred.\n> Use *${usedPrefix}report* to report it.\n\n${error.message}`, m)
 }}
 
-handler.tags = ['descargas']
+handler.tags = ['media']
 handler.help = ['apkmod']
 handler.command = ['apk', 'modapk', 'aptoide']
 handler.group = true

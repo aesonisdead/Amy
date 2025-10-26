e5e7946f47c80ab0eca93d39710949a7cca8c749
aesonisdead <vuuvuuvvuvybuu@gmail.com>
2025-10-26 15:46:01 +0100

    Update group-admins.js

--- plugins/group-admins.js
+++ plugins/group-admins.js
@@ -7,18 +7,18 @@ const listAdmin = groupAdmins.map(v => `● @${v.id.split('@')[0]}`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 const pesan = args.join` `
 const oi = `» ${pesan}`
-const text = `『✦』Admins del grupo:  
+const text = `『✦』Group admins:  
   
 ${listAdmin}
 
-❍ Mensaje ${oi || 'Sin especificar'}`
+❍ Message ${oi || 'Unspecified'}`
 conn.sendFile(m.chat, pp, 'error.jpg', text, m, false, {mentions: [...groupAdmins.map((v) => v.id), owner]})
 }
 
 handler.help = ['admins']
 handler.tags = ['grupo']
-handler.customPrefix = /a|@/i
-handler.command = /^(admins|@admins|dmins)$/i
+handler.customPrefix = /\/|@/i  // allow / or @ as prefix
+handler.command = /^(admins|a)$/i  // match "admins" or "a" exactly
 handler.group = true
 
 export default handler

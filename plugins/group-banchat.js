926e0f93017b2b4d400cf39a6765373a583569ab
aesonisdead <vuuvuuvvuvybuu@gmail.com>
2025-10-26 05:33:54 +0100

    Update group-banchat.js

--- plugins/group-banchat.js
+++ plugins/group-banchat.js
@@ -2,26 +2,26 @@ let handler = async (m, { conn, usedPrefix, command, args }) => {
 let chat = global.db.data.chats[m.chat]
 if (command === 'bot') {
 if (args.length === 0) {
-const estado = chat.isBanned ? '✗ Desactivado' : '✓ Activado'
-const info = `「✦」Un administrador puede activar o desactivar a *${botname}* utilizando:\n\n✐ _Activar_ » *${usedPrefix}bot enable*\n✐ _Desactivar_ » *${usedPrefix}bot disable*\n\n✧ Estado actual » *${estado}*`
+const estado = chat.isBanned ? '✗ Deactivated' : '✓ Activated'
+const info = `「✦」An administrator can enable or disable *${botname}* using:\n\n✐ _Activate_ » *${usedPrefix}bot enable*\n✐ _Deactivate_ » *${usedPrefix}bot disable*\n\n✧ Current status » *${estado}*`
 return conn.reply(m.chat, info, m)
 }
 if (args[0] === 'off') {
 if (chat.isBanned) {
-return conn.reply(m.chat, `《✦》${botname} ya estaba desactivado.`, m)
+return conn.reply(m.chat, `《✦》${botname} is already disabled.`, m)
 }
 chat.isBanned = true
-return conn.reply(m.chat, `❀ Has *desactivado* a ${botname}!`, m)
+return conn.reply(m.chat, `❀ You have *deactivated* ${botname}!`, m)
 } else if (args[0] === 'on') {
 if (!chat.isBanned) {
-return conn.reply(m.chat, `《✦》${botname} ya estaba activado.`, m)
+return conn.reply(m.chat, `《✦》${botname} is already activated.`, m)
 }
 chat.isBanned = false
-return conn.reply(m.chat, `❀ Has *activado* a ${botname}!`, m)
+return conn.reply(m.chat, `❀ You have *activated* ${botname}!`, m)
 }}}
 
 handler.help = ['bot']
-handler.tags = ['grupo']
+handler.tags = ['group']
 handler.command = ['bot']
 handler.admin = true
 

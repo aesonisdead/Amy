28c15bd90ce41e86a648b7ffc0d7f110d8ab9eb9
aesonisdead <vuuvuuvvuvybuu@gmail.com>
2025-10-26 05:29:50 +0100

    Update group-add.js

--- plugins/group-add.js
+++ plugins/group-add.js
@@ -1,17 +1,17 @@
 import moment from 'moment-timezone'
 
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `❀ Por favor, ingrese el número al que quiere enviar una invitación al grupo.`, m)
-if (text.includes('+')) return conn.reply(m.chat, `ꕥ Ingrese el número todo junto sin el *+*`, m)
-if (isNaN(text)) return conn.reply(m.chat, `ꕥ Ingrese sólo números sin su código de país y sin espacios.`, m)
+if (!text) return conn.reply(m.chat, `❀ Please enter the number you want to send a group invitation to..`, m)
+if (text.includes('+')) return conn.reply(m.chat, `ꕥ Enter the number all together without the *+*`, m)
+if (isNaN(text)) return conn.reply(m.chat, `ꕥ Enter only numbers without your country code and without spaces.`, m)
 let group = m.chat
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-let tag = m.sender ? '@' + m.sender.split('@')[0] : 'Usuario'
-const chatLabel = m.isGroup ? (await conn.getName(m.chat) || 'Grupal') : 'Privado'
-const horario = `${moment.tz('America/Caracas').format('DD/MM/YYYY hh:mm:ss A')}`
-const invite = `❀ 𝗜𝗡𝗩𝗜𝗧𝗔𝗖𝗜𝗢𝗡 𝗔 𝗨𝗡 𝗚𝗥𝗨𝗣𝗢\n\nꕥ *Usuario* » ${tag}\n✿ *Chat* » ${chatLabel}\n✰ *Fecha* » ${horario}\n✦ *Link* » ${link}`
+let tag = m.sender ? '@' + m.sender.split('@')[0] : 'User'
+const chatLabel = m.isGroup ? (await conn.getName(m.chat) || 'Group') : 'Private'
+const horario = `${moment.tz('Africa/Casablanca').format('DD/MM/YYYY hh:mm:ss A')}`
+const invite = `❀ 𝗚𝗥𝗢𝗨𝗣 𝗜𝗡𝗩𝗜𝗧𝗔𝗧𝗜𝗢𝗡\n\nꕥ *User* » ${tag}\n✿ *Chat* » ${chatLabel}\n✰ *Date* » ${horario}\n✦ *Link* » ${link}`
 await conn.reply(`${text}@s.whatsapp.net`, invite, m, { mentions: [m.sender] })
-m.reply(`❀ El enlace de invitación fue enviado al usuario correctamente.`)
+m.reply(`❀ The invitation link was sent to the user successfully..`)
 }
 
 handler.help = ['invite']

9ce9f2d75c263fcb4eb7014e86af877c4db5467a
aesonisdead <vuuvuuvvuvybuu@gmail.com>
2025-10-26 19:35:32 +0100

    Update group-infogrupo.js

--- plugins/group-infogrupo.js
+++ plugins/group-infogrupo.js
@@ -2,42 +2,42 @@ import { promises as fs } from 'fs'
 
 const handler = async (m, {conn, participants, groupMetadata}) => {
 const chat = global.db.data.chats[m.chat]
-const pp = await conn.profilePictureUrl(m.chat, 'image').catch(() => 'https://files.catbox.moe/xr2m6u.jpg')
+const pp = await conn.profilePictureUrl(m.chat, 'image').catch(() => 'https://files.catbox.moe/mq2yh8.jpg')
 const { antiLink, detect, welcome, sWelcome, sBye, modoadmin, nsfw, isBanned, economy, gacha, primaryBot } = global.db.data.chats[m.chat]
 const groupAdmins = participants.filter(p => p.admin)
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
-const creador = (!owner || owner.startsWith('1203') || owner.length < 15) ? 'No encontrado' : `@${owner.split('@')[0]}`
+const creador = (!owner || owner.startsWith('1203') || owner.length < 15) ? 'Not found' : `@${owner.split('@')[0]}`
 const rawPrimary = typeof chat.primaryBot === 'string' ? chat.primaryBot : '';
-const botprimary = rawPrimary.endsWith('@s.whatsapp.net') ? `@${rawPrimary.split('@')[0]}` : 'Aleatorio';  
+const botprimary = rawPrimary.endsWith('@s.whatsapp.net') ? `@${rawPrimary.split('@')[0]}` : 'Random';  
 const totalreg = Object.keys(global.db.data.users).length
 
-const text = `「✦」Grupo ◢ ${groupMetadata.subject} ◤
+const text = `「✦」Group ◢ ${groupMetadata.subject} ◤
 
-❀ *Creador* » ${creador}
-✦ *Miembros* » ${participants.length} Participantes
+❀ *Creator* » ${creador}
+✦ *Members* » ${participants.length} Participants
 ꕥ *Admins* » ${groupAdmins.length}
-☆ *Registrados* » ${totalreg.toLocaleString()}
-❖ *Bot principal* » ${botprimary}
+☆ *Registered* » ${totalreg.toLocaleString()}
+❖ *Main bot* » ${botprimary}
 
-*▢ Opciones:*
-> ◆ *${botname}* » ${isBanned ? '✗ Desactivado' : '✓ Activado'}
-> ◆ *Welcome* » ${welcome ? '✓ Activado' : '✗ Desactivado'}
-> ◆ *Alertas* » ${detect ? '✓ Activado' : '✗ Desactivado'}
-> ◆ *Anti-Link* » ${antiLink ? '✓ Activado' : '✗ Desactivado'}
-> ◆ *Only-Admin* » ${modoadmin ? '✓ Activado' : '✗ Desactivado'}
-> ◆ *NSFW* » ${nsfw ? '✓ Activado' : '✗ Desactivado'}
-> ◆ *Gacha* » ${gacha ? '✓ Activado' : '✗ Desactivado'}
-> ◆ *Economy* » ${economy ? '✓ Activado' : '✗ Desactivado'}
+*▢ Options:*
+> ◆ *${botname}* » ${isBanned ? '✗ Disabled' : '✓ Activated'}
+> ◆ *Welcome* » ${welcome ? '✓ Activated' : '✗ Disabled'}
+> ◆ *Alerts* » ${detect ? '✓ Activated' : '✗ Disabled'}
+> ◆ *Anti-Link* » ${antiLink ? '✓ Activated' : '✗ Disabled'}
+> ◆ *Only-Admin* » ${modoadmin ? '✓ Activated' : '✗ Disabled'}
+> ◆ *NSFW* » ${nsfw ? '✓ Activated' : '✗ Disabled'}
+> ◆ *Gacha* » ${gacha ? '✓ Activated' : '✗ Disabled'}
+> ◆ *Economy* » ${economy ? '✓ Activated' : '✗ Disabled'}
 
-*▢ Mensajes:*
-> ● *Welcome* » ${(sWelcome || 'Sin mensaje de bienvenida').replace(/{usuario}/g, `@${m.sender.split('@')[0]}`).replace(/{grupo}/g, `*${groupMetadata.subject}*`).replace(/{desc}/g, `*${groupMetadata.desc || 'Sin descripción'}*`)}
-> ● *Bye* » ${(sBye || 'Sin mensaje de despedida').replace(/{usuario}/g, `@${m.sender.split('@')[0]}`).replace(/{grupo}/g, `*${groupMetadata.subject}*`).replace(/{desc}/g, `*${groupMetadata.desc || 'Sin descripción'}*`)}`
+*▢ Messages:*
+> ● *Welcome* » ${(sWelcome || 'No welcome message').replace(/{user}/g, `@${m.sender.split('@')[0]}`).replace(/{group}/g, `*${groupMetadata.subject}*`).replace(/{desc}/g, `*${groupMetadata.desc || 'No description'}*`)}
+> ● *Bye* » ${(sBye || 'No farewell message').replace(/{user}/g, `@${m.sender.split('@')[0]}`).replace(/{group}/g, `*${groupMetadata.subject}*`).replace(/{desc}/g, `*${groupMetadata.desc || 'No description'}*`)}`
 conn.sendFile(m.chat, pp, 'img.jpg', text, m, false, { mentions: [owner, rawPrimary, m.sender] })
 }
 
-handler.help = ['infogrupo']
-handler.tags = ['grupo']
-handler.command = ['infogrupo', 'gp']
+handler.help = ['infogroup']
+handler.tags = ['group']
+handler.command = ['infogroup', 'gp']
 handler.group = true
 
 export default handler

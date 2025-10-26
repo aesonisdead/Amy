c020872ba6fcfb705cba9e627408f19054a14feb
aesonisdead <vuuvuuvvuvybuu@gmail.com>
2025-10-26 14:56:08 +0100

    Update group-demote.js

--- plugins/group-demote.js
+++ plugins/group-demote.js
@@ -1,22 +1,22 @@
 var handler = async (m, { conn, usedPrefix, command, text, groupMetadata }) => {
 let mentionedJid = await m.mentionedJid
 let user = mentionedJid && mentionedJid.length ? mentionedJid[0] : m.quoted && await m.quoted.sender ? await m.quoted.sender : null
-if (!user) return conn.reply(m.chat, `❀ Debes mencionar a un usuario para poder promoverlo a administrador.`, m)
+if (!user) return conn.reply(m.chat, `❀ You must mention a user to be able to promote them to administrator.`, m)
 try {
 const groupInfo = await conn.groupMetadata(m.chat)
 const ownerGroup = groupInfo.owner || m.chat.split`-`[0] + '@s.whatsapp.net'
 const ownerBot = global.owner[0][0] + '@s.whatsapp.net'
-if (user === conn.user.jid) return conn.reply(m.chat, `ꕥ No puedes degradar al bot.`, m)
-if (user === ownerGroup) return conn.reply(m.chat, `ꕥ No puedes degradar al creador del grupo.`, m)
-if (user === ownerBot) return conn.reply(m.chat, `ꕥ No puedes degradar al propietario del bot.`, m)
+if (user === conn.user.jid) return conn.reply(m.chat, `ꕥ You can't demote the bot.`, m)
+if (user === ownerGroup) return conn.reply(m.chat, `ꕥ You cannot demote the group creator.`, m)
+if (user === ownerBot) return conn.reply(m.chat, `ꕥ You cannot demote the bot owner.`, m)
 await conn.groupParticipantsUpdate(m.chat, [user], 'demote')
-conn.reply(m.chat, `❀ Fue descartado como admin.`, m)
+conn.reply(m.chat, `❀ User is now no longer an admin.`, m)
 } catch (e) {
-conn.reply(m.chat, `⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}report* para informarlo.\n\n${e.message}`, m)
+conn.reply(m.chat, `⚠︎ A problem has occurred.\n> Use *${usedPrefix}report* to report it.\n\n${e.message}`, m)
 }}
 
 handler.help = ['demote']
-handler.tags = ['grupo']
+handler.tags = ['group']
 handler.command = ['demote', 'degradar']
 handler.group = true
 handler.admin = true

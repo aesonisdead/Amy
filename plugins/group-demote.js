addb61021fa6008b965d85efe2b2d3eb7dd0836a
aesonisdead <vuuvuuvvuvybuu@gmail.com>
2025-10-26 16:23:00 +0100

    Update group-demote.js

--- plugins/group-demote.js
+++ plugins/group-demote.js
@@ -1,7 +1,7 @@
 var handler = async (m, { conn, usedPrefix, command, text, groupMetadata }) => {
 let mentionedJid = await m.mentionedJid
 let user = mentionedJid && mentionedJid.length ? mentionedJid[0] : m.quoted && await m.quoted.sender ? await m.quoted.sender : null
-if (!user) return conn.reply(m.chat, `❀ You must mention a user to be able to demote them from administrator.`, m)
+if (!user) return conn.reply(m.chat, `❀ You must mention an user to be able to demote them from administrator.`, m)
 try {
 const groupInfo = await conn.groupMetadata(m.chat)
 const ownerGroup = groupInfo.owner || m.chat.split`-`[0] + '@s.whatsapp.net'
@@ -10,7 +10,7 @@ if (user === conn.user.jid) return conn.reply(m.chat, `ꕥ You can't demote the 
 if (user === ownerGroup) return conn.reply(m.chat, `ꕥ You cannot demote the group creator.`, m)
 if (user === ownerBot) return conn.reply(m.chat, `ꕥ You cannot demote the bot owner.`, m)
 await conn.groupParticipantsUpdate(m.chat, [user], 'demote')
-conn.reply(m.chat, `❀ User is now no longer an admin.`, m)
+conn.reply(m.chat, `❀ The user is no longer an admin.`, m)
 } catch (e) {
 conn.reply(m.chat, `⚠︎ A problem has occurred.\n> Use *${usedPrefix}report* to report it.\n\n${e.message}`, m)
 }}

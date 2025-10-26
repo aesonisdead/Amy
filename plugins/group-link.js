eea5a380c1ad3dfb095985af3770bee5e9ef2f1c
aesonisdead <vuuvuuvvuvybuu@gmail.com>
2025-10-26 20:08:42 +0100

    Update group-link.js

--- plugins/group-link.js
+++ plugins/group-link.js
@@ -1,13 +1,13 @@
 var handler = async (m, { conn, args }) => {
 let group = m.chat
-const pp = await conn.profilePictureUrl(group, 'image').catch((_) => 'https://files.catbox.moe/xr2m6u.jpg')
+const pp = await conn.profilePictureUrl(group, 'image').catch((_) => 'https://files.catbox.moe/mq2yh8.jpg')
 let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-let message = `\t*⌁☍꒷₊˚ Link del Grupo ꒷₊˚⌁☍*\n\n> \`Link:\` ${link}`
+let message = `\t*⌁☍꒷₊˚ Group Link ꒷₊˚⌁☍*\n\n> \`Link:\` ${link}`
 await conn.sendMessage(group, { image: { url: pp }, caption: message })
 }
 
 handler.help = ['link']
-handler.tags = ['grupo']
+handler.tags = ['group']
 handler.command = ['link', 'enlace']
 handler.group = true
 handler.botAdmin = true

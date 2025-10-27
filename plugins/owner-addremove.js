b0521a8bf7017a97f5877d63479753a0ac38fb69
aesonisdead <vuuvuuvvuvybuu@gmail.com>
2025-10-27 11:10:02 +0100

    Update owner-addremove.js

--- plugins/owner-addremove.js
+++ plugins/owner-addremove.js
@@ -46,8 +46,10 @@ const premArgs = text.replace(/^@\S+\s*/, '').trim().split(' ').filter(arg => ar
 if (premArgs.length < 2) return m.reply('ꕥ Send a valid time\n> Example (1h, 2d, 3w, 4m).')
 await m.react('🕒')
 let tiempo = 0
-const cant = parseInt(premArgs[0])
-const unidad = premArgs[1]
+const match = premArgs[0].match(/(\d+)([hdwm])/)
+if (!match) return m.reply('ꕥ Invalid time.\nOptions:\n h = hours, d = days, w = weeks, m = months')
+const cant = parseInt(match[1])
+const unidad = match[2]
 if (unidad === 'h') tiempo = 3600000 * cant
 else if (unidad === 'd') tiempo = 86400000 * cant
 else if (unidad === 'w') tiempo = 604800000 * cant

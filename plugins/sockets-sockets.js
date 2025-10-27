1b673a0408462f1d75f6f690fcd2a3e408f7a4b8
aesonisdead <vuuvuuvvuvybuu@gmail.com>
2025-10-27 12:20:28 +0100

    Update sockets-sockets.js

--- plugins/sockets-sockets.js
+++ plugins/sockets-sockets.js
@@ -7,41 +7,41 @@ const linkRegex = /https:\/\/chat\.whatsapp\.com\/([0-9A-Za-z]{20,24})/i
 const handler = async (m, { conn, command, usedPrefix, text }) => {
 try {
 const isSubBots = [conn.user.jid, ...global.owner.map(number => `${number}@s.whatsapp.net`)].includes(m.sender)
-if (!isSubBots) return m.reply(`❀ El comando *${command}* solo puede ser ejecutado por el Socket.`)
+if (!isSubBots) return m.reply(`❀ The command *${command}* can only be executed by the owner.`)
 switch (command) {
-case 'self': case 'public': case 'antiprivado': case 'antiprivate': case 'gponly': case 'sologp': {
+case 'self': case 'public': case 'private': case 'antiprivate': case 'gponly': case 'sologp': {
 const config = global.db.data.settings[conn.user.jid]
 const value = text ? text.trim().toLowerCase() : ''
-const type = /self|public/.test(command) ? 'self' : /antiprivado|antiprivate/.test(command) ? 'antiPrivate' : /gponly|sologp/.test(command) ? 'gponly' : null
-if (!type) return m.reply(`ꕥ Modo no reconocido.`)
+const type = /self|public/.test(command) ? 'self' : /private|antiprivate/.test(command) ? 'antiPrivate' : /gponly|sologp/.test(command) ? 'gponly' : null
+if (!type) return m.reply(`ꕥ Mode not recognized.`)
 const isEnable = config[type] || false
 const enable = value === 'enable' || value === 'on'
 const disable = value === 'disable' || value === 'off'
 if (enable || disable) {
 if (isEnable === enable)
-return m.reply(`ꕥ El modo *${type}* ya estaba ${enable ? 'activado' : 'desactivado'}.`)
+return m.reply(`ꕥ The mode *${type}* was already ${enable ? 'activated' : 'disabled'}.`)
 config[type] = enable
-return conn.reply(m.chat, `❀ Has *${enable ? 'activado' : 'desactivado'}* el modo *${type}* para el Socket.`, m)
+return conn.reply(m.chat, `❀ *${enable ? 'activated' : 'deactivated'}* the mode *${type}* by the owner.`, m)
 }
-conn.reply(m.chat, `「✦」Puedes activar o desactivar el modo *${type}* utilizando:\n\n● Activar » ${usedPrefix}${command} enable\n● Desactivar » ${usedPrefix}${command} disable\n\n✧ Estado actual » *${isEnable ? '✓ Activado' : '✗ Desactivado'}*`, m)
+conn.reply(m.chat, `「✦」You can turn the mode on or off *${type}* using:\n\n● Activate » ${usedPrefix}${command} enable\n● Deactivate » ${usedPrefix}${command} disable\n\n✧ Current status » *${isEnable ? '✓ Activated' : '✗ Deactivated'}*`, m)
 break
 }
-case 'join': {
-if (!text) return m.reply(`❀ Debes enviar un enlace de invitación para unirme a un grupo.`)
+case 'join': case 'j': {
+if (!text) return m.reply(`❀ You must send an invitation link to join a group..`)
 const [_, code] = text.match(linkRegex) || []
-if (!code) return m.reply(`ꕥ El enlace de invitación no es válido.`)
+if (!code) return m.reply(`ꕥ The invitation link is not valid.`)
 await m.react('🕒')
 await conn.groupAcceptInvite(code)
 await m.react('✔️')
-m.reply(`❀ ${botname} se a unido exitosamente al grupo.`)
+m.reply(`❀ ${botname} has successfully joined the group.`)
 break
 }
-case 'salir': case 'leave': {
+case 'l': case 'leave': {
 await m.react('🕒')
 const id = text || m.chat
 const chat = global.db.data.chats[m.chat]
 chat.welcome = false
-await conn.reply(id, `❀ Adiós a todos, ${botname} se despide! (≧ω≦)ゞ`)
+await conn.reply(id, `❀ Goodbye everyone, ${botname} says goodbye! (≧ω≦)ゞ`)
 await conn.groupLeave(id)
 chat.welcome = true
 await m.react('✔️')
@@ -52,10 +52,10 @@ const rawId = conn.user?.id || ''
 const cleanId = jidDecode(rawId)?.user || rawId.split('@')[0]
 const index = global.conns?.findIndex(c => c.user.jid === m.sender)
 if (global.conn.user.jid === conn.user.jid)
-return conn.reply(m.chat, '❀ Este comando está deshabilitado en las sesiones principales.', m)
+return conn.reply(m.chat, '❀ This command is disabled in main sessions.', m)
 if (index === -1 || !global.conns[index])
-return conn.reply(m.chat, '⚠︎ La sesión ya está cerrada o no se encontró una conexión activa.', m)
-conn.reply(m.chat, '✩ Tu sesión ha sido cerrada exitosamente.', m)
+return conn.reply(m.chat, '⚠︎ The session is already closed or an active connection was not found.', m)
+conn.reply(m.chat, '✩ Your session has been successfully closed.', m)
 setTimeout(async () => {
 await global.conns[index].logout()
 global.conns.splice(index, 1)
@@ -70,21 +70,21 @@ case 'reload': {
 const rawId = conn.user?.id || ''
 const cleanId = jidDecode(rawId)?.user || rawId.split('@')[0]
 const sessionPath = path.join(global.jadi, cleanId)
-if (!fs.existsSync(sessionPath)) return conn.reply(m.chat, '❀ Este comando solo puede ejecutarse desde una instancia Sub-Bot.', m)
+if (!fs.existsSync(sessionPath)) return conn.reply(m.chat, '❀ This command can only be executed from a Sub-Bot instance.', m)
 await m.react('🕒')
 if (typeof global.reloadHandler !== 'function')
-throw new Error('No se encontró la función global.reloadHandler')
+throw new Error('The global.reloadHandler function was not found.')
 await global.reloadHandler(true)
 await m.react('✔️')
-conn.reply(m.chat, '✿ La sesión fue recargada correctamente.', m)
+conn.reply(m.chat, '✿ The session was successfully reloaded.', m)
 break
 }}} catch (error) {
 await m.react('✖️')
-conn.reply(m.chat, `⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}report* para informarlo.\n\n${error.message || error}`, m)
+conn.reply(m.chat, `⚠︎ A problem has occurred.\n> Use *${usedPrefix}report* para informarlo.\n\n${error.message || error}`, m)
 }}
 
-handler.command = ['self', 'public', 'antiprivate', 'gponly', 'sologp', 'join', 'salir', 'leave', 'logout', 'reload']
-handler.help = ['self', 'public', 'antiprivate', 'gponly', 'sologp', 'join', 'salir', 'leave', 'logout', 'reload']
+handler.command = ['self', 'public', 'antiprivate', 'gponly', 'sologp', 'join', 'j', 'l', 'leave', 'logout', 'reload']
+handler.help = ['self', 'public', 'antiprivate', 'gponly', 'sologp', 'join', 'j', 'l', 'leave', 'logout', 'reload']
 handler.tags = ['socket']
 
 export default handler

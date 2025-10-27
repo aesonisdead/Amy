1346a5eaeba355fb12fc097a02bbab823eed684c
aesonisdead <vuuvuuvvuvybuu@gmail.com>
2025-10-27 13:04:45 +0100

    Update sockets-editsub.js

--- plugins/sockets-editsub.js
+++ plugins/sockets-editsub.js
@@ -2,42 +2,42 @@ import fetch from 'node-fetch'
 import Jimp from 'jimp'
 
 const handler = async (m, { conn, command, usedPrefix, text }) => {
-const isSubBots = [conn.user.jid, ...global.owner.map(([number]) => `${number}@s.whatsapp.net`)].includes(m.sender)
-if (!isSubBots) return m.reply(`❀ El comando *${command}* solo puede ser ejecutado por el Socket.`)
+const isSubBots = [conn.user.jid, ...global.owner.map(number => `${number}@s.whatsapp.net`)].includes(m.sender)
+if (!isSubBots) return m.reply(`❀ The command *${command}* can only be executed by the owner.`)
 try {
 const value = text ? text.trim() : ''
 switch (command) {
 case 'setpfp': case 'setimage': {
 const q = m.quoted || m
 const mime = (q.msg || q).mimetype || ''
-if (!/image\/(png|jpe?g)/.test(mime)) return conn.reply(m.chat, `❀ Por favor, responde o envía una imagen válida para cambiar la foto de perfil.`, m)
+if (!/image\/(png|jpe?g)/.test(mime)) return conn.reply(m.chat, `❀ Please reply or submit a valid image to change your profile picture..`, m)
 const media = await q.download()
-if (!media) return conn.reply(m.chat, `ꕥ No se pudo obtener la imagen.`, m)
+if (!media) return conn.reply(m.chat, `ꕥ The image could not be obtained.`, m)
 const image = await Jimp.read(media)
 const buffer = await image.getBufferAsync(Jimp.MIME_JPEG)
 await conn.updateProfilePicture(conn.user.jid, buffer)
-conn.reply(m.chat, `❀ Se cambió la *foto de perfil* del Socket correctamente.`, m)
+conn.reply(m.chat, `❀ Socket *profile picture* has been changed successfully.`, m)
 break
 }
 case 'setstatus': case 'setbio': {
-if (!text) return conn.reply(m.chat, `❀ Por favor, ingresa la nueva biografía que deseas ponerme.`, m)
+if (!text) return conn.reply(m.chat, `❀ Please enter the new bio you want to put on me.`, m)
 await conn.updateProfileStatus(text)
-conn.reply(m.chat, `❀ Se cambió la biografía del Socket a *"${text}"* correctamente.`, m)
+conn.reply(m.chat, `❀ Socket's bio was changed to *"${text}"* successfully.`, m)
 break
 }
 case 'setusername': case 'setuser': {
-if (!value) return conn.reply(m.chat, '❀ Ingresa el nuevo nombre de usuario que deseas establecer.', m)
+if (!value) return conn.reply(m.chat, '❀ Enter the new username you want to set.', m)
 if (value.length < 3 || value.length > 25)
-return conn.reply(m.chat, 'ꕥ El nombre debe tener entre 3 y 25 caracteres.')
+return conn.reply(m.chat, 'ꕥ The name must be between 3 and 25 characters..')
 await conn.updateProfileName(value)
-m.reply(`❀ Se cambió el nombre de usuario a *${value}* correctamente.`)
+m.reply(`❀ Username changed to *${value}* successfully.`)
 break
 }}} catch (error) {
-m.reply(`⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}report* para informarlo.\n\n${error.message}`)
+m.reply(`⚠︎ A problem has occurred.\n> Use *${usedPrefix}report* to report it.\n\n${error.message}`)
 }}
 
 handler.help = ['setpfp', 'setimage', 'setstatus', 'setbio', 'setusername', 'setuser']
 handler.tags = ['socket']
 handler.command = ['setpfp', 'setimage', 'setstatus', 'setbio', 'setusername', 'setuser']
 
-export default handlerault handler
+export default handler

5d09e1460a74ef1a053c32c671a13c5eca8b1275
aesonisdead <vuuvuuvvuvybuu@gmail.com>
2025-10-27 10:22:36 +0100

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,7 +1,7 @@
 const handler = async (m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
 const pesan = args.join` `
 const oi = `*» INFO :* ${pesan}`
-let teks = `*!  MENCION GENERAL  !*\n  *PARA ${participants.length} MIEMBROS* 🗣️\n\n ${oi}\n\n╭  ┄ 𝅄 ۪꒰ \`⡞᪲=͟͟͞${botname}≼᳞ׄ\` ꒱ ۟ 𝅄 ┄\n`
+let teks = `*!  GENERAL MENTION  !*\n  *FOR ${participants.length} MEMBERS* 🗣️\n\n ${oi}\n\n╭  ┄ 𝅄 ۪꒰ \`⡞᪲=͟͟͞${botname}≼᳞ׄ\` ꒱ ۟ 𝅄 ┄\n`
 for (const mem of participants) {
 teks += `┊ꕥ @${mem.id.split('@')[0]}\n`
 }
@@ -9,9 +9,9 @@ teks += `╰⸼ ┄ ┄ ┄ ─  ꒰  ׅ୭ *${vs}* ୧ ׅ ꒱  ┄  ─ ┄⸼`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map((a) => a.id) })
 }
 
-handler.help = ['todos']
+handler.help = ['all']
 handler.tags = ['group']
-handler.command = ['todos', 'invocar', 'tagall']
+handler.command = ['all', 'tagall']
 handler.admin = true
 handler.group = true
 

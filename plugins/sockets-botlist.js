f650e3ba975f58f3a450480de19ce559b51d4a68
aesonisdead <vuuvuuvvuvybuu@gmail.com>
2025-10-27 12:54:56 +0100

    Update sockets-botlist.js

--- plugins/sockets-botlist.js
+++ plugins/sockets-botlist.js
@@ -12,10 +12,10 @@ const segRest = segundos % 60
 const minRest = minutos % 60
 const horasRest = horas % 24
 let resultado = ""
-if (días) resultado += `${días} días, `
-if (horasRest) resultado += `${horasRest} horas, `
-if (minRest) resultado += `${minRest} minutos, `
-if (segRest) resultado += `${segRest} segundos`
+if (días) resultado += `${días} days, `
+if (horasRest) resultado += `${horasRest} hours, `
+if (minRest) resultado += `${minRest} minutes, `
+if (segRest) resultado += `${segRest} seconds`
 return resultado.trim()
 }
 let groupBots = users.filter((bot) => participants.some((p) => p.id === bot))
@@ -25,19 +25,19 @@ const isMainBot = bot === global.conn.user.jid
 const v = global.conns.find((conn) => conn.user.jid === bot)
 const uptime = isMainBot ? convertirMsADiasHorasMinutosSegundos(Date.now() - global.conn.uptime) : v?.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : "Activo desde ahora"
 const mention = bot.replace(/[^0-9]/g, '')
-return `@${mention}\n> Bot: ${isMainBot ? 'Principal' : 'Sub-Bot'}\n> Online: ${uptime}`}).join("\n\n") : `✧ No hay bots activos en este grupo`
-const message = `*「 ✦ 」 Lista de bots activos*
+return `@${mention}\n> Bot: ${isMainBot ? 'Principal' : 'Sub-Bot'}\n> Online: ${uptime}`}).join("\n\n") : `✧ There are no active bots in this group.`
+const message = `*「 ✦ 」 List of active bots*
 
 ❀ Principal: *1*
 ✿ Subs: *${users.length - 1}*
 
-❏ En este grupo: *${groupBots.length}* bots
+❏ In this group: *${groupBots.length}* bots
 ${botsGroup}`
 const mentionList = groupBots.map(bot => bot.endsWith("@s.whatsapp.net") ? bot : `${bot}@s.whatsapp.net`)
 rcanal.contextInfo.mentionedJid = mentionList
 await conn.sendMessage(m.chat, { text: message, ...rcanal }, { quoted: m })
 } catch (error) {
-m.reply(`⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}report* para informarlo.\n\n${error.message}`)
+m.reply(`⚠︎ A problem has occurred.\n> Use *${usedPrefix}report* to report it.\n\n${error.message}`)
 }}
 
 handler.tags = ["serbot"]
